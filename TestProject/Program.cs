ec9229d19744729cfadc4e17ef140fbfafc167fc
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-06-01 09:13:05 +0200

    #2 - Legger til nytt prosjekt

---
+++ TestProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing release notes");
+            var line = Console.ReadLine();
+            Console.WriteLine("You have typed: " + line);
+            Console.ReadLine();
+        }
+    }
+}
