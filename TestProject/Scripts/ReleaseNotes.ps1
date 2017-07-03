18369a20e3e89f1f3844eef1f1b2bebfd8b6d336
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-07-03 13:04:54 +0200

    #1 - Tester å liste opp tags fra github

--- TestProject/Scripts/ReleaseNotes.ps1
+++ TestProject/Scripts/ReleaseNotes.ps1
@@ -1,13 +1,18 @@
 Param(
-[string] $token,
+[string] $gToken,
 [string] $gApiUrl,
 [string] $gProjectUrl,
 [string] $hookUri
 )
-$f = "Release notes:"
+$f = "Release notes for build $(Build.BuildNumber) $(Build.BuildURI):"
+
+$tagsUri = $gApiUrl/tags
+$gTags = Invoke-RestMethod -Method Get -Uri $tagsUri -Header @{Authorization = "token $gToken"}
+Write-Host $gTags 
+
 git log "test-v1335..test-v1389" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
 $gU="$gApiUrl/issues/$_"
-$i=Invoke-RestMethod -Method Get -Uri $gU -Header @{Authorization = "token $token"}
+$i=Invoke-RestMethod -Method Get -Uri $gU -Header @{Authorization = "token $gToken"}
 $t=$i.title
 $l="* #[$_]($gProjectUrl/issues/$_) $t" 
 $f="$f `n $l"}
