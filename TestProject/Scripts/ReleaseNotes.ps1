7201bd7f2aebcc9d43f85905c63a610cb348d5b8
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-07-04 13:15:25 +0200

    #1 - Variabler til versjoner

--- TestProject/Scripts/ReleaseNotes.ps1
+++ TestProject/Scripts/ReleaseNotes.ps1
@@ -9,8 +9,10 @@ $f = "Release notes for build $env:BUILD_BUILDNUMBER $env:BUILD_BUILDURI :"
 $tagsUri = "$gApiUrl/tags"
 $gTags = Invoke-RestMethod -Method Get -Uri $tagsUri -Header @{Authorization = "token $gToken"}
 $gTags | Select-Object -first 2 | ForEach {$lastVersions = $lastVersions+$_.name}
+$latestVersion = $lastVersions[0]
+$previousVersion = $lastVersions[1]
 
-git log "$lastVersions[1]..$lastVersions[0]" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
+git log "$previousVersion..$latestVersion" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
 $gU="$gApiUrl/issues/$_"
 $i=Invoke-RestMethod -Method Get -Uri $gU -Header @{Authorization = "token $gToken"}
 $t=$i.title
