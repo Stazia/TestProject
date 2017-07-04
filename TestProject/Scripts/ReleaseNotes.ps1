c39fdbe15308ea7e5c6ac7b0cc7e64756b238cd6
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-07-04 12:55:04 +0200

    #1 - Prøver på nytt

--- TestProject/Scripts/ReleaseNotes.ps1
+++ TestProject/Scripts/ReleaseNotes.ps1
@@ -10,7 +10,7 @@ $tagsUri = "$gApiUrl/tags"
 $gTags = Invoke-RestMethod -Method Get -Uri $tagsUri -Header @{Authorization = "token $gToken"}
 $gTags | Select-Object -first 2 | ForEach {$lastVersions = $lastVersions+$_.name}
 
-git log "$lastVersions[0]..$lastVersions[1]" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
+git log "$lastVersions[1]..$lastVersions[0]" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
 $gU="$gApiUrl/issues/$_"
 $i=Invoke-RestMethod -Method Get -Uri $gU -Header @{Authorization = "token $gToken"}
 $t=$i.title
