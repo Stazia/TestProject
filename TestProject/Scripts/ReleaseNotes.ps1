1dbc5fef90d1617c97accccf59a742c885e4d8fb
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-07-04 12:50:19 +0200

    #1 - Versjoner hentes fra github api

--- TestProject/Scripts/ReleaseNotes.ps1
+++ TestProject/Scripts/ReleaseNotes.ps1
@@ -8,9 +8,9 @@ $f = "Release notes for build $env:BUILD_BUILDNUMBER $env:BUILD_BUILDURI :"
 
 $tagsUri = "$gApiUrl/tags"
 $gTags = Invoke-RestMethod -Method Get -Uri $tagsUri -Header @{Authorization = "token $gToken"}
-$gTags | ForEach Write-Host $.name
+$gTags | Select-Object -first 2 | ForEach {$lastVersions = $lastVersions+$_.name}
 
-git log "test-v1335..test-v1389" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
+git log "$lastVersions[0]..$lastVersions[1]" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
 $gU="$gApiUrl/issues/$_"
 $i=Invoke-RestMethod -Method Get -Uri $gU -Header @{Authorization = "token $gToken"}
 $t=$i.title
