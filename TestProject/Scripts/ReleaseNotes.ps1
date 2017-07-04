b6b9f68da7567b88954105b354b6c8b14ae7032d
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-07-04 14:37:36 +0200

    #2 - Tekst forbedringer

--- TestProject/Scripts/ReleaseNotes.ps1
+++ TestProject/Scripts/ReleaseNotes.ps1
@@ -12,13 +12,13 @@ $gTags | Select-Object -first 2 | ForEach {$lastVersions = $lastVersions+$_.name
 $latestVersion = $lastVersions[0]
 $previousVersion = $lastVersions[1]
 
-$notes = "Ny versjon ($latestVersion) er ute i prod! Dette er fikset siden sist:"
+$notes = "Ny versjon av Stadnamn ($latestVersion) er ute i produksjon! `n Dette er fikset siden sist:"
 
 git log "$previousVersion..$latestVersion" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
 $gUrl = "$gApiUrl/issues/$_"
 $item = Invoke-RestMethod -Method Get -Uri $gUrl -Header @{Authorization = "token $gToken"}
 $title = $item.title
-$line = "* Issue $_($gProjectUrl/issues/$_) $title" 
+$line = "* Issue $_ ($gProjectUrl/issues/$_) $title" 
 $notes = "$notes `n $line"}
 Write-Host $notes
 
