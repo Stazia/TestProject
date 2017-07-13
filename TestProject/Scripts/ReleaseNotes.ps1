52ba8df19a8a5959c34d538f2fd65493dc45ab97
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-07-13 13:39:23 +0200

    STAD-27: Refaktorering

--- TestProject/Scripts/ReleaseNotes.ps1
+++ TestProject/Scripts/ReleaseNotes.ps1
@@ -1,13 +1,13 @@
 Param(
-[string] $gToken,
-[string] $gApiUrl,
+[string] $githubToken,
+[string] $githubApiUrl,
 [string] $gProjectUrl,
 [string] $sHookUri
 )
 
-$tagsUri = "$gApiUrl/tags"
+$tagsUri = "$githubApiUrl/tags"
 $lastVersions = @()
-$gTags = Invoke-RestMethod -Method Get -Uri $tagsUri -Header @{Authorization = "token $gToken"}
+$gTags = Invoke-RestMethod -Method Get -Uri $tagsUri -Header @{Authorization = "token $githubToken"}
 $gTags | Select-Object -first 2 | ForEach {$lastVersions = $lastVersions+$_.name}
 $latestVersion = $lastVersions[0]
 $previousVersion = $lastVersions[1]
@@ -15,8 +15,8 @@ $previousVersion = $lastVersions[1]
 $notes = "Ny versjon av Stadnamn ($latestVersion) er ute i produksjon! `n Dette er fikset siden sist:"
 
 git log "$previousVersion..$latestVersion" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
-$gUrl = "$gApiUrl/issues/$_"
-$item = Invoke-RestMethod -Method Get -Uri $gUrl -Header @{Authorization = "token $gToken"}
+$gUrl = "$githubApiUrl/issues/$_"
+$item = Invoke-RestMethod -Method Get -Uri $gUrl -Header @{Authorization = "token $githubToken"}
 $title = $item.title
 $line = "* Issue $_ ($gProjectUrl/issues/$_) $title" 
 $notes = "$notes `n $line"}
