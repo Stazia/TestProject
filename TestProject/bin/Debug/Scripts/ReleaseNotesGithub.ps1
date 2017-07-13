1e8873da006607f22c4c2b5fc021fbcc4f43c07d
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-07-13 14:34:54 +0200

    STAD-22 - Inkludert filer i proj

---
+++ TestProject/bin/Debug/Scripts/ReleaseNotesGithub.ps1
@@ -0,0 +1,26 @@
+Param(
+[string] $githubToken,
+[string] $githubApiUri,
+[string] $githubProjectUri,
+[string] $slackHookUri
+)
+
+$tagsUri = "$githubApiUri/tags"
+$lastVersions = @()
+$gTags = Invoke-RestMethod -Method Get -Uri $tagsUri -Header @{Authorization = "token $githubToken"}
+$gTags | Select-Object -first 2 | ForEach {$lastVersions = $lastVersions+$_.name}
+$latestVersion = $lastVersions[0]
+$previousVersion = $lastVersions[1]
+
+$notes = "Ny versjon av Stadnamn ($latestVersion) er ute i produksjon! `n Dette er fikset siden sist:"
+
+git log "$previousVersion..$latestVersion" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
+$gUrl = "$githubApiUri/issues/$_"
+$item = Invoke-RestMethod -Method Get -Uri $gUrl -Header @{Authorization = "token $githubToken"}
+$title = $item.title
+$line = "* Issue $_ ($githubProjectUri/issues/$_) $title" 
+$notes = "$notes `n $line"}
+Write-Host $notes
+
+$slackMessage = @{ "text" = $notes } | ConvertTo-Json -Compress
+Invoke-WebRequest -UseBasicParsing -Body $slackMessage -Method POST -Uri $slackHookUri
