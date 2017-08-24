c1dcbd524fb72633e227b5befdf4ddd453d109d9
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-08-24 14:36:05 +0200

    STAD-23 - Dette er en test commit (RDF)

--- TestProject/Scripts/ReleaseNotesGithub.ps1
+++ TestProject/Scripts/ReleaseNotesGithub.ps1
@@ -16,8 +16,8 @@ $previousVersion = $lastVersions[1]
 $notes = "Ny versjon av Stadnamn ($latestVersion) er ute i produksjon! `n Dette er fikset siden sist:"
 
 git log "$previousVersion..$latestVersion" --extended-regexp --pretty=oneline --no-merges | Select-String -Pattern "#[0-9]+" | ForEach {$_.Matches.Value.Trim("#")} | Select-Object -unique | ForEach {
-$gUrl = "$githubApiUri/issues/$_"
-$item = Invoke-RestMethod -Method Get -Uri $gUrl -Header @{Authorization = "token $githubToken"}
+$githubUrl = "$githubApiUri/issues/$_"
+$item = Invoke-RestMethod -Method Get -Uri $githubUrl -Header @{Authorization = "token $githubToken"}
 $title = $item.title
 $line = "* Issue $_ ($githubProjectUri/issues/$_) $title" 
 $notes = "$notes `n $line"}
