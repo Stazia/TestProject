8804b007b63279f7707433c90b2336574d571d1c
Anastasia Poroshina <anastasia.poroshina@gmail.com>
2017-08-29 13:31:04 +0200

    STAD-23 - Test commit

--- TestProject/Scripts/ReleaseNotesJira.ps1
+++ TestProject/Scripts/ReleaseNotesJira.ps1
@@ -33,7 +33,7 @@ Write-Host $notes
 $slackMessage = @{ "text" = $notes } | ConvertTo-Json -Compress
 Invoke-WebRequest -UseBasicParsing -Body $slackMessage -Method POST -Uri $slackHookUri
 
-$messages = '-m ""'
+$messages = '-m " "'
 $notes | Foreach-Object { $m = ' -m ' + '"' + $_ + '"' ; $messages = $messages + $m }
 
 git tag $latestVersion -f $messages
